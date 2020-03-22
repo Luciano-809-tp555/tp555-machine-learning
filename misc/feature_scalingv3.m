2013783e7464ae5f657686d3137a882393e252bc
zz4fap <zz4fap@gmail.com>
2020-03-22 20:47:03 -0300

    new scripts

--- misc/feature_scalingv3.m
+++ misc/feature_scalingv3.m
@@ -2,23 +2,41 @@ clear all;close all;clc
 
 fontSize = 14;
 
-if(0)
-    
-    a_int = [-20;-20];
-    
-    M = 100000;
-    
-    x1 = 1*randn(M, 1);
-    
-    x2 = 1000*randn(M, 1) + 1000;
-    
-    y = x1 + x2;
-    
-    a1 = -20:1/1:24;
-    
-    a2 = -20:1/1:24;
-    
-    [A1,A2] = meshgrid(a1,a2);
+% Initial parameters.
+a_int = [-20;-20];
+
+M = 100000;
+
+% Training set.
+x1 = rand(M, 1);
+
+x2 = 10*rand(M, 1) + 10;
+
+y = x1 + x2;
+
+% Prediction set.
+Mp = M;
+x1p = rand(Mp, 1);
+
+x2p = 10*rand(Mp, 1) + 10;
+
+yp = x1p + x2p;
+
+% Parameter space.
+a1 = -20:1/1:24;
+
+a2 = -20:1/1:24;
+
+[A1,A2] = meshgrid(a1,a2);
+
+
+non_scalled = 1;
+standardization = 1;
+target_standardization = 0;
+minmaxscaling = 1;
+target_minmaxscaling = 0;
+
+if(non_scalled==1)
     
     J = zeros(length(A1),length(A2));
     for iter1=1:1:length(a1)
@@ -51,7 +69,7 @@ if(0)
     Joptimum = (1/M)*sum((y - yhat).^2);
     
     %% Gradient-descent solution.
-    alpha = 0.0000002;
+    alpha = 0.002;
     
     % Initialize 'a' at a random location within the parameter's space.
     a(:,1) = a_int;
@@ -66,7 +84,7 @@ if(0)
         
         h = a(1,iter)*x1 + a(2,iter)*x2;
         
-        update = -(2./M).*(y - h).'*[x1 x2];
+        update = -(2./M).*(y - h).'*X;
         
         a(:,iter+1) = a(:,iter) - alpha.*update.';
         
@@ -79,6 +97,11 @@ if(0)
         iter = iter + 1;
         
     end
+    
+    fprintf(1, 'Training error: %1.4e\n', Jgd(iter));
+    yhatp = a(1,iter)*x1p + a(2,iter)*x2p;
+    Jgdp = (1/Mp).*sum((yp - yhatp).^2);
+    fprintf(1, 'Prediction error: %1.4e\n', Jgdp);
     
     figure2 = figure('rend','painters','pos',[10 10 800 700]);
     subplot(2,1,1);
@@ -100,11 +123,12 @@ if(0)
     xlabel('Iteracao', 'FontSize', fontSize)
     ylabel('J_e', 'FontSize', fontSize)
     title('Iteracoes vs. Erro', 'FontSize', fontSize)
-    lgd = legend('\alpha = 1e-7', 'Interpreter', 'latex');
+    strlgd = sprintf('$\\alpha = %1.0e$', alpha);
+    lgd = legend({strlgd}, 'Interpreter', 'latex');
     lgd.FontSize = 14;
     xlim([0 iter-1])
     grid on
-
+    
 end
 
 
@@ -115,31 +139,23 @@ end
 
 
 
-if(1)
+
+if(standardization==1)
+    clear a Jgd
     
-    a_int = [-20;-20];
-    
-    M = 100000;
-    
-    x1 = randn(M, 1);
-    
-    x2 = 1000*randn(M, 1) + 1000;
-    
-    y = x1 + x2;
+    x1_mean = mean(x1);
+    x1_std = std(x1);
+    x1 = (x1 - x1_mean) ./ (x1_std);
     
     x2_mean = mean(x2);
     x2_std = std(x2);
     x2 = (x2 - x2_mean) ./ (x2_std);
     
-    y_mean = mean(y);
-    y_std = std(y);
-    y = (y - y_mean) ./ (y_std);    
-     
-    a1 = -20:1/1:24;
-    
-    a2 = -20:1/1:24;
-    
-    [A1, A2] = meshgrid(a1,a2);
+    if(target_standardization==1)
+        y_mean = mean(y);
+        y_std = std(y);
+        y = (y - y_mean) ./ (y_std);
+    end
     
     J = zeros(length(A1),length(A2));
     for iter1=1:1:length(a1)
@@ -159,7 +175,7 @@ if(1)
     xlabel('a_1', 'FontSize', fontSize)
     ylabel('a_2', 'FontSize', fontSize)
     zlabel('J_e', 'FontSize', fontSize)
-    title('Superf�cie de Erro', 'FontSize', fontSize)
+    title('Superficie de Erro', 'FontSize', fontSize)
     
     %% Closed-form solution.
     
@@ -187,7 +203,7 @@ if(1)
         
         h = a(1,iter)*x1 + a(2,iter)*x2;
         
-        update = -(2./M).*(y - h).'*[x1 x2];
+        update = -(2./M).*(y - h).'*X;
         
         a(:,iter+1) = a(:,iter) - alpha.*update.';
         
@@ -201,13 +217,25 @@ if(1)
         
     end
     
+    fprintf(1, 'Training error: %1.4e\n', Jgd(iter));
+    
+    x1p = (x1p - x1_mean) ./ x1_std;
+    x2p = (x2p - x2_mean) ./ x2_std;
+    if(target_standardization==1)
+        yp = (yp - y_mean) ./ (y_std);
+    end
+    
+    yhatp = a(1,iter)*x1p + a(2,iter)*x2p;
+    Jgdp = (1/Mp).*sum((yp - yhatp).^2);
+    fprintf(1, 'Prediction error: %1.4e\n', Jgdp);
+    
     figure2 = figure('rend','painters','pos',[10 10 800 700]);
     subplot(2,1,1);
     contour(A1, A2, J, 'ShowText','on')
     xlabel('a_1', 'FontSize', fontSize)
     ylabel('a_2', 'FontSize', fontSize)
     zlabel('J_e', 'FontSize', fontSize)
-    title('Contorno da Superf�cie de Erro', 'FontSize', fontSize)
+    title('Contorno da Superficie de Erro', 'FontSize', fontSize)
     hold on
     %scatter3(a_opt(1), a_opt(2), Joptimum, 'r*', 'MarkerSize', 10)
     %scatter3(a(1,:), a(2,:), Jgd, 'kx', 'MarkerSize', 10);
@@ -218,14 +246,15 @@ if(1)
     %figure3 = figure('rend','painters','pos',[10 10 800 700]);
     subplot(2,1,2);
     semilogy(0:1:iter-1, Jgd, 'LineWidth',1)
-    xlabel('Itera��o', 'FontSize', fontSize)
+    xlabel('Iteracao', 'FontSize', fontSize)
     ylabel('J_e', 'FontSize', fontSize)
-    title('Itera��es vs. Erro', 'FontSize', fontSize)
-    lgd = legend('\alpha = 1e-7', 'Interpreter', 'latex');
+    title('Iteracoes vs. Erro', 'FontSize', fontSize)
+    strlgd = sprintf('$\\alpha = %1.0e$', alpha);
+    lgd = legend({strlgd}, 'Interpreter', 'latex');
     lgd.FontSize = 14;
     xlim([0 iter-1])
     grid on
-
+    
 end
 
 
@@ -244,27 +273,22 @@ end
 
 
 
-if(0)
+if(minmaxscaling==1)
+    clear a Jgd
     
-    a_int = [-20;-20];
+    x1_min = min(x1);
+    x1_max = max(x1);
+    x1 = (x1 - x1_min) ./ (x1_max - x1_min);
     
-    M = 100000;
+    x2_min = min(x2);
+    x2_max = max(x2);
+    x2 = (x2 - x2_min) ./ (x2_max - x2_min);
     
-    x1 = randn(M, 1);
-    
-    x2 = 1000*randn(M, 1) + 1000;
-    
-    y = x1 + x2;
-    
-    x2_mean = mean(x2);
-    x2_std = std(x2);
-    x2 = (x2 - x2_mean) ./ (x2_std);
-     
-    a1 = -20:1/1:24;
-    
-    a2 = -20:1/1:24;
-    
-    [A1,A2] = meshgrid(a1,a2);
+    if(target_minmaxscaling==1)
+        y_min = min(y);
+        y_max = max(y);
+        y = (y - y_min) ./ (y_max - y_min);
+    end
     
     J = zeros(length(A1),length(A2));
     for iter1=1:1:length(a1)
@@ -284,7 +308,7 @@ if(0)
     xlabel('a_1', 'FontSize', fontSize)
     ylabel('a_2', 'FontSize', fontSize)
     zlabel('J_e', 'FontSize', fontSize)
-    title('Superf�cie de Erro', 'FontSize', fontSize)
+    title('Superficie de Erro', 'FontSize', fontSize)
     
     %% Closed-form solution.
     
@@ -297,8 +321,7 @@ if(0)
     Joptimum = (1/M)*sum((y - yhat).^2);
     
     %% Gradient-descent solution.
-    alpha = 0.4;
-    
+    alpha = 0.8;
     % Initialize 'a' at a random location within the parameter's space.
     a(:,1) = a_int;
     
@@ -312,7 +335,7 @@ if(0)
         
         h = a(1,iter)*x1 + a(2,iter)*x2;
         
-        update = -(2./M).*(y - h).'*[x1 x2];
+        update = -(2./M).*(y - h).'*X;
         
         a(:,iter+1) = a(:,iter) - alpha.*update.';
         
@@ -326,13 +349,23 @@ if(0)
         
     end
     
+    fprintf(1, 'Training error: %1.4e\n', Jgd(iter));
+    x1p = (x1p - x1_min) ./ (x1_max - x1_min);
+    x2p = (x2p - x2_min) ./ (x2_max - x2_min);
+    if(target_minmaxscaling==1)
+        yp = (yp - y_min) ./ (y_max - y_min);
+    end
+    yhatp = a(1,iter)*x1p + a(2,iter)*x2p;
+    Jgdp = (1/Mp).*sum((yp - yhatp).^2);
+    fprintf(1, 'Prediction error: %1.4e\n', Jgdp);
+    
     figure2 = figure('rend','painters','pos',[10 10 800 700]);
     subplot(2,1,1);
     contour(A1, A2, J, 'ShowText','on')
     xlabel('a_1', 'FontSize', fontSize)
     ylabel('a_2', 'FontSize', fontSize)
     zlabel('J_e', 'FontSize', fontSize)
-    title('Contorno da Superf�cie de Erro', 'FontSize', fontSize)
+    title('Contorno da Superficie de Erro', 'FontSize', fontSize)
     hold on
     %scatter3(a_opt(1), a_opt(2), Joptimum, 'r*', 'MarkerSize', 10)
     %scatter3(a(1,:), a(2,:), Jgd, 'kx', 'MarkerSize', 10);
@@ -343,12 +376,13 @@ if(0)
     %figure3 = figure('rend','painters','pos',[10 10 800 700]);
     subplot(2,1,2);
     semilogy(0:1:iter-1, Jgd, 'LineWidth',1)
-    xlabel('Itera��o', 'FontSize', fontSize)
+    xlabel('Iteracao', 'FontSize', fontSize)
     ylabel('J_e', 'FontSize', fontSize)
-    title('Itera��es vs. Erro', 'FontSize', fontSize)
-    lgd = legend('\alpha = 1e-7', 'Interpreter', 'latex');
+    title('Iteracoes vs. Erro', 'FontSize', fontSize)
+    strlgd = sprintf('$\\alpha = %1.0e$', alpha);
+    lgd = legend({strlgd}, 'Interpreter', 'latex');
     lgd.FontSize = 14;
     xlim([0 iter-1])
     grid on
-
+    
 end
--- misc/lista2_exercicio8.m
+++ misc/lista2_exercicio8.m
@@ -1,24 +1,174 @@
 clear all;close all;clc
 
+rng(01012019);
+
 fontSize = 14;
 
-if(0)
+% Initial parameters.
+a_int = [-20;-20];
+
+M = 100000;
+
+% Training set.
+x1t = randn(M, 1);
+
+x2t = 10*randn(M, 1) + 10;
+
+yt = x1t + x2t;
+
+% Prediction set.
+rng(12041988);
+
+Mp = M;
+x1p = randn(Mp, 1);
+
+x2p = 10*randn(Mp, 1) + 10;
+
+yp = x1p + x2p;
+
+% Parameter space.
+a1 = -44:1/1:48;
+
+a2 = -44:1/1:48;
+
+[A1,A2] = meshgrid(a1,a2);
+
+
+non_scaled = 1;
+standardization = 1;
+minmaxscaling = 1;
+
+target_standardization = 1;
+target_minmaxscaling = 1;
+
+
+if(non_scaled==1)
+    clear a Jgd x1 x2 y x1pp x2pp ypp
     
-    a_int = [-20;-20];
+    x1 = x1t;
+    x2 = x2t;
+    y = yt;
     
-    M = 100000;
+    J = zeros(length(A1),length(A2));
+    for iter1=1:1:length(a1) 
+        for iter2=1:1:length(a2)
+            yhat = A1(iter1, iter2)*x1 + A2(iter1, iter2)*x2;           
+            J(iter1, iter2) = (1/length(y)).*sum((y - yhat).^2);
+        end
+    end
     
-    x1 = 1*randn(M, 1);
+    figure1 = figure('rend','painters','pos',[10 10 800 700]);
+    surf(A1, A2, J)
+    xlabel('a_1', 'FontSize', fontSize)
+    ylabel('a_2', 'FontSize', fontSize)
+    zlabel('J_e', 'FontSize', fontSize)
+    title('Superficie de Erro', 'FontSize', fontSize)
     
-    x2 = 1000*randn(M, 1) + 1000;
+    %% Closed-form solution.
     
-    y = x1 + x2;
+    X = [x1 x2];
     
-    a1 = -20:1/1:24;
+    a_opt = pinv(X.'*X)*X.'*y;
     
-    a2 = -20:1/1:24;
+    yhat = a_opt(1)*x1 + a_opt(2)*x2;
     
-    [A1,A2] = meshgrid(a1,a2);
+    Joptimum = (1/M)*sum((y - yhat).^2);
+    
+    %% Gradient-descent solution.
+    alpha = 0.002;
+    
+    % Initialize 'a' at a random location within the parameter's space.
+    a(:,1) = a_int;
+    
+    yhat = a(1,1)*x1 + a(2,1)*x2;
+    
+    Jgd(1) = (1/M)*sum((y - yhat).^2);
+    
+    error = 1;
+    iter = 1;
+    while(error > 0.001 && iter <= 10000)
+        
+        h = a(1,iter)*x1 + a(2,iter)*x2;
+        
+        update = -(2./M).*(y - h).'*X;
+        
+        a(:,iter+1) = a(:,iter) - alpha.*update.';
+        
+        yhat = a(1,iter+1)*x1 + a(2,iter+1)*x2;
+        
+        Jgd(iter+1) = (1/M).*sum((y - yhat).^2);
+        
+        error = abs(Jgd(iter)-Jgd(iter+1));
+        
+        iter = iter + 1;
+        
+    end
+    
+    %fprintf(1, 'a1/a2: %1.2e\n', a_opt(1)/a_opt(2));
+    %fprintf(1, 'a1/a2: %1.2e\n', a(1, iter)/a(2,iter));
+    
+    fprintf(1, 'Training error: %1.4e\n', Jgd(iter));
+    yhatp = a(1,iter)*x1p + a(2,iter)*x2p;
+    Jgdpp = (1/Mp).*sum((yp - yhatp).^2);
+    fprintf(1, 'Prediction error: %1.4e\n', Jgdpp);
+    
+    figure2 = figure('rend','painters','pos',[10 10 800 700]);
+    subplot(2,1,1);
+    contour(A1, A2, J, 'ShowText','on')
+    xlabel('a_1', 'FontSize', fontSize)
+    ylabel('a_2', 'FontSize', fontSize)
+    zlabel('J_e', 'FontSize', fontSize)
+    title('Contorno da Superficie de Erro', 'FontSize', fontSize)
+    hold on
+    %scatter3(a_opt(1), a_opt(2), Joptimum, 'r*', 'MarkerSize', 10)
+    %scatter3(a(1,:), a(2,:), Jgd, 'kx', 'MarkerSize', 10);
+    plot(a_opt(1), a_opt(2), 'r*', 'MarkerSize', 10, 'LineWidth',1);
+    plot(a(1,:), a(2,:), 'kx', 'MarkerSize', 10, 'LineWidth',1);
+    hold off;
+    
+    %figure3 = figure('rend','painters','pos',[10 10 800 700]);
+    subplot(2,1,2);
+    semilogy(0:1:iter-1, Jgd, 'LineWidth',1)
+    xlabel('Iteracao', 'FontSize', fontSize)
+    ylabel('J_e', 'FontSize', fontSize)
+    title('Iteracoes vs. Erro', 'FontSize', fontSize)
+    strlgd = sprintf('$\\alpha = %1.0e$', alpha);
+    lgd = legend({strlgd}, 'Interpreter', 'latex');
+    lgd.FontSize = 14;
+    xlim([0 iter-1])
+    grid on
+    
+end
+
+
+
+
+
+
+
+
+
+
+if(standardization==1)
+    clear a Jgd x1 x2 y x1pp x2pp ypp
+    
+    x1 = x1t;
+    x2 = x2t;
+    y = yt;
+    
+    x1_mean = mean(x1);
+    x1_std = std(x1);
+    x1 = (x1 - x1_mean) ./ (x1_std);
+    
+    x2_mean = mean(x2);
+    x2_std = std(x2);
+    x2 = (x2 - x2_mean) ./ (x2_std);
+    
+    if(target_standardization==1)
+        y_mean = mean(y);
+        y_std = std(y);
+        y = (y - y_mean) ./ (y_std);
+    end
     
     J = zeros(length(A1),length(A2));
     for iter1=1:1:length(a1)
@@ -51,7 +201,7 @@ if(0)
     Joptimum = (1/M)*sum((y - yhat).^2);
     
     %% Gradient-descent solution.
-    alpha = 0.0000002;
+    alpha = 0.5;
     
     % Initialize 'a' at a random location within the parameter's space.
     a(:,1) = a_int;
@@ -66,7 +216,7 @@ if(0)
         
         h = a(1,iter)*x1 + a(2,iter)*x2;
         
-        update = -(2./M).*(y - h).'*[x1 x2];
+        update = -(2./M).*(y - h).'*X;
         
         a(:,iter+1) = a(:,iter) - alpha.*update.';
         
@@ -79,6 +229,21 @@ if(0)
         iter = iter + 1;
         
     end
+    
+    %fprintf(1, 'a1/a2: %1.2e\n', a_opt(1)/a_opt(2));
+    %fprintf(1, 'a1/a2: %1.2e\n', a(1, iter)/a(2,iter));
+    
+    fprintf(1, 'Training error: %1.4e\n', Jgd(iter));
+    x1pp = (x1p - x1_mean) ./ x1_std;
+    x2pp = (x2p - x2_mean) ./ x2_std;
+    if(target_standardization==1)
+        ypp = (yp - y_mean) ./ (y_std);
+    else
+        ypp = yp;
+    end
+    yhatp = a(1,iter)*x1pp + a(2,iter)*x2pp;
+    Jgdpp = (1/Mp).*sum((ypp - yhatp).^2);
+    fprintf(1, 'Prediction error: %1.4e\n', Jgdpp);
     
     figure2 = figure('rend','painters','pos',[10 10 800 700]);
     subplot(2,1,1);
@@ -100,11 +265,12 @@ if(0)
     xlabel('Iteracao', 'FontSize', fontSize)
     ylabel('J_e', 'FontSize', fontSize)
     title('Iteracoes vs. Erro', 'FontSize', fontSize)
-    lgd = legend('\alpha = 1e-7', 'Interpreter', 'latex');
+    strlgd = sprintf('$\\alpha = %1.0e$', alpha);
+    lgd = legend({strlgd}, 'Interpreter', 'latex');
     lgd.FontSize = 14;
     xlim([0 iter-1])
     grid on
-
+    
 end
 
 
@@ -115,31 +281,34 @@ end
 
 
 
-if(1)
+
+
+
+
+
+
+
+
+if(minmaxscaling==1)
+    clear a Jgd x1 x2 y x1pp x2pp ypp
     
-    a_int = [-20;-20];
+    x1 = x1t;
+    x2 = x2t;
+    y = yt;
     
-    M = 100000;
+    x1_min = min(x1);
+    x1_max = max(x1);
+    x1 = (x1 - x1_min) ./ (x1_max - x1_min);
     
-    x1 = randn(M, 1);
+    x2_min = min(x2);
+    x2_max = max(x2);
+    x2 = (x2 - x2_min) ./ (x2_max - x2_min);
     
-    x2 = 1000*randn(M, 1) + 1000;
-    
-    y = x1 + x2;
-    
-    x2_mean = mean(x2);
-    x2_std = std(x2);
-    x2 = (x2 - x2_mean) ./ (x2_std);
-    
-    y_mean = mean(y);
-    y_std = std(y);
-    y = (y - y_mean) ./ (y_std);    
-     
-    a1 = -20:1/1:24;
-    
-    a2 = -20:1/1:24;
-    
-    [A1, A2] = meshgrid(a1,a2);
+    if(target_minmaxscaling==1)
+        y_min = min(y);
+        y_max = max(y);
+        y = (y - y_min) ./ (y_max - y_min);
+    end
     
     J = zeros(length(A1),length(A2));
     for iter1=1:1:length(a1)
@@ -159,7 +328,7 @@ if(1)
     xlabel('a_1', 'FontSize', fontSize)
     ylabel('a_2', 'FontSize', fontSize)
     zlabel('J_e', 'FontSize', fontSize)
-    title('Superf�cie de Erro', 'FontSize', fontSize)
+    title('Superficie de Erro', 'FontSize', fontSize)
     
     %% Closed-form solution.
     
@@ -172,8 +341,7 @@ if(1)
     Joptimum = (1/M)*sum((y - yhat).^2);
     
     %% Gradient-descent solution.
-    alpha = 0.5;
-    
+    alpha = 0.9;
     % Initialize 'a' at a random location within the parameter's space.
     a(:,1) = a_int;
     
@@ -187,7 +355,7 @@ if(1)
         
         h = a(1,iter)*x1 + a(2,iter)*x2;
         
-        update = -(2./M).*(y - h).'*[x1 x2];
+        update = -(2./M).*(y - h).'*X;
         
         a(:,iter+1) = a(:,iter) - alpha.*update.';
         
@@ -201,13 +369,28 @@ if(1)
         
     end
     
+    %fprintf(1, 'a1/a2: %1.2e\n', a_opt(1)/a_opt(2));
+    %fprintf(1, 'a1/a2: %1.2e\n', a(1, iter)/a(2,iter));
+    
+    fprintf(1, 'Training error: %1.4e\n', Jgd(iter));
+    x1pp = (x1p - x1_min) ./ (x1_max - x1_min);
+    x2pp = (x2p - x2_min) ./ (x2_max - x2_min);
+    if(target_minmaxscaling==1)
+        ypp = (yp - y_min) ./ (y_max - y_min);
+    else
+        ypp = yp;
+    end
+    yhatp = a(1,iter)*x1pp + a(2,iter)*x2pp;
+    Jgdpp = (1/Mp).*sum((ypp - yhatp).^2);
+    fprintf(1, 'Prediction error: %1.4e\n', Jgdpp);
+    
     figure2 = figure('rend','painters','pos',[10 10 800 700]);
     subplot(2,1,1);
     contour(A1, A2, J, 'ShowText','on')
     xlabel('a_1', 'FontSize', fontSize)
     ylabel('a_2', 'FontSize', fontSize)
     zlabel('J_e', 'FontSize', fontSize)
-    title('Contorno da Superf�cie de Erro', 'FontSize', fontSize)
+    title('Contorno da Superficie de Erro', 'FontSize', fontSize)
     hold on
     %scatter3(a_opt(1), a_opt(2), Joptimum, 'r*', 'MarkerSize', 10)
     %scatter3(a(1,:), a(2,:), Jgd, 'kx', 'MarkerSize', 10);
@@ -218,137 +401,13 @@ if(1)
     %figure3 = figure('rend','painters','pos',[10 10 800 700]);
     subplot(2,1,2);
     semilogy(0:1:iter-1, Jgd, 'LineWidth',1)
-    xlabel('Itera��o', 'FontSize', fontSize)
+    xlabel('Iteracao', 'FontSize', fontSize)
     ylabel('J_e', 'FontSize', fontSize)
-    title('Itera��es vs. Erro', 'FontSize', fontSize)
-    lgd = legend('\alpha = 1e-7', 'Interpreter', 'latex');
+    title('Iteracoes vs. Erro', 'FontSize', fontSize)
+    strlgd = sprintf('$\\alpha = %1.0e$', alpha);
+    lgd = legend({strlgd}, 'Interpreter', 'latex');
     lgd.FontSize = 14;
     xlim([0 iter-1])
     grid on
-
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if(0)
     
-    a_int = [-20;-20];
-    
-    M = 100000;
-    
-    x1 = randn(M, 1);
-    
-    x2 = 1000*randn(M, 1) + 1000;
-    
-    y = x1 + x2;
-    
-    x2_mean = mean(x2);
-    x2_std = std(x2);
-    x2 = (x2 - x2_mean) ./ (x2_std);
-     
-    a1 = -20:1/1:24;
-    
-    a2 = -20:1/1:24;
-    
-    [A1,A2] = meshgrid(a1,a2);
-    
-    J = zeros(length(A1),length(A2));
-    for iter1=1:1:length(a1)
-        
-        for iter2=1:1:length(a2)
-            
-            yhat = A1(iter1, iter2)*x1 + A2(iter1, iter2)*x2;
-            
-            J(iter1, iter2) = (1/length(y)).*sum((y - yhat).^2);
-            
-        end
-        
-    end
-    
-    figure1 = figure('rend','painters','pos',[10 10 800 700]);
-    surf(A1, A2, J)
-    xlabel('a_1', 'FontSize', fontSize)
-    ylabel('a_2', 'FontSize', fontSize)
-    zlabel('J_e', 'FontSize', fontSize)
-    title('Superf�cie de Erro', 'FontSize', fontSize)
-    
-    %% Closed-form solution.
-    
-    X = [x1 x2];
-    
-    a_opt = pinv(X.'*X)*X.'*y;
-    
-    yhat = a_opt(1)*x1 + a_opt(2)*x2;
-    
-    Joptimum = (1/M)*sum((y - yhat).^2);
-    
-    %% Gradient-descent solution.
-    alpha = 0.4;
-    
-    % Initialize 'a' at a random location within the parameter's space.
-    a(:,1) = a_int;
-    
-    yhat = a(1,1)*x1 + a(2,1)*x2;
-    
-    Jgd(1) = (1/M)*sum((y - yhat).^2);
-    
-    error = 1;
-    iter = 1;
-    while(error > 0.001 && iter <= 10000)
-        
-        h = a(1,iter)*x1 + a(2,iter)*x2;
-        
-        update = -(2./M).*(y - h).'*[x1 x2];
-        
-        a(:,iter+1) = a(:,iter) - alpha.*update.';
-        
-        yhat = a(1,iter+1)*x1 + a(2,iter+1)*x2;
-        
-        Jgd(iter+1) = (1/M).*sum((y - yhat).^2);
-        
-        error = abs(Jgd(iter)-Jgd(iter+1));
-        
-        iter = iter + 1;
-        
-    end
-    
-    figure2 = figure('rend','painters','pos',[10 10 800 700]);
-    subplot(2,1,1);
-    contour(A1, A2, J, 'ShowText','on')
-    xlabel('a_1', 'FontSize', fontSize)
-    ylabel('a_2', 'FontSize', fontSize)
-    zlabel('J_e', 'FontSize', fontSize)
-    title('Contorno da Superf�cie de Erro', 'FontSize', fontSize)
-    hold on
-    %scatter3(a_opt(1), a_opt(2), Joptimum, 'r*', 'MarkerSize', 10)
-    %scatter3(a(1,:), a(2,:), Jgd, 'kx', 'MarkerSize', 10);
-    plot(a_opt(1), a_opt(2), 'r*', 'MarkerSize', 10, 'LineWidth',1);
-    plot(a(1,:), a(2,:), 'kx', 'MarkerSize', 10, 'LineWidth',1);
-    hold off;
-    
-    %figure3 = figure('rend','painters','pos',[10 10 800 700]);
-    subplot(2,1,2);
-    semilogy(0:1:iter-1, Jgd, 'LineWidth',1)
-    xlabel('Itera��o', 'FontSize', fontSize)
-    ylabel('J_e', 'FontSize', fontSize)
-    title('Itera��es vs. Erro', 'FontSize', fontSize)
-    lgd = legend('\alpha = 1e-7', 'Interpreter', 'latex');
-    lgd.FontSize = 14;
-    xlim([0 iter-1])
-    grid on
-
 end
